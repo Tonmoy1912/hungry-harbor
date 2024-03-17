4bb8d75c9308fc92154dc2aba6ac3f2819f314ea
Tonmoy <tonmoybiswas19122002@gmail.com>
2024-03-17 15:29:58 +0530

    .gitignore file modified

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
-let num=4.567;
-console.log(num.toPrecision(3));
+const curDate=new Date();
+// console.log(curDate.toDateString());
+// console.log(curDate.toLocaleDateString());
+
+function getFormatedDate(date){
+    let str=date.toDateString();
+    let arr=str.split(" ");
+    return arr[2]+" "+arr[1];
+}
+console.log(getFormatedDate(new Date()));
